4077b5e7b0c76077dd1f803f59496997b64a29ba
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2021-08-14 23:20:15 +0100

    Change language from PT to EN and add sc.nextLine.

--- workspace/primeiro-programa-java/src/br/com/dio/Calculadora.java
+++ workspace/primeiro-programa-java/src/br/com/dio/Calculadora.java
@@ -6,41 +6,48 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner (System.in);
+    	Scanner sc = new Scanner (System.in);
 		
 		double a, b;
 		
-		System.out.println("Please, type the first number: ");
+		System.out.print("Please, type the first number: ");
 		a = sc.nextDouble();
+		sc.nextLine(); //this method from scanner is to consume the next line
 		
-		System.out.println("Please, type a second number: ");
+		
+		System.out.print("Please, type a second number: ");
 		b = sc.nextDouble();
+		System.out.println();
 		
-		double soma = soma(a,b);
-		double subtracao = subtracao(a,b);
-		double multiplicacao = multiplicacao(a,b);
-		double divisao = divisao(a,b);
+		double sum = sum(a,b);
+		double subtraction = subtraction(a,b);
+		double multiplication = multiplication(a,b);
+		double division = division(a,b);
 		
-		System.out.println("soma: " + soma);
-		System.out.println("subtracão: " + subtracao);
-		System.out.println("multiplicação: " + multiplicacao);
-		System.out.println("divisão: " + divisao);
+		System.out.println("Results:");
+		System.out.println("sum: " + sum);
+		System.out.println("subtraction: " + subtraction);
+		System.out.println("multiplication: " + multiplication);
+		System.out.println("division: " + division);
+	
+		sc.close();
+		
 
 	}
 
-	public static double soma (double a, double b) {
+	public static double sum (double a, double b) {
 		return a + b;
 	}
 	
-	public static double subtracao (double a, double b){
+	public static double subtraction (double a, double b){
 		return a - b;
 	}
 	
-	public static double multiplicacao (double a, double b){
+	public static double multiplication (double a, double b){
 		return a * b;
 	}
 	
-	public static double divisao (double a, double b){
+	public static double division (double a, double b){
 		return a / b;
 	}
 

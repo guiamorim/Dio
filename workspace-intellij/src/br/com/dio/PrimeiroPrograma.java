04b8957f35e5bf578ec0ee51bbfba8572dba7ca0
Guilherme Amorim <guilherme.asfreitas@gmail.com>
2021-08-15 21:46:58 +0100

    Add class livro1

--- workspace-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ workspace-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,12 +8,50 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-       /* System.out.println("Hello World!");
+        Livro livro1 = new Livro( "O Problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+
+        /* System.out.println("Hello World!");
 
         int a = 5;
         int b = 3;
 
         System.out.println(a+b);*/
 
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
